134c6a29c31dc3e149c7b5bbc41129d54dc1c55b
ambidere <xanthiandere@gmail.com>
2017-08-21 08:07:29 +0800

    Adjusted for Heroku deployment

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ var config = {
     },
     twitter : {
         consumer_key : process.env.TWITTER_CONSUMER_KEY,
-        consumer_secret : process.env.TWITTER_CONSUMER_SECRET,
+        consumer_secret : process.env.TWITTER_CONSUMER_KEY_SECRET,
         access_token : process.env.TWITTER_ACCESS_TOKEN,
         access_token_secret : process.env.TWITTER_ACCESS_TOKEN_SECRET
     }
